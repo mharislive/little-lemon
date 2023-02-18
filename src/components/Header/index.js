440c997376042d9f70d1cb122be6d1d96fe1a652
mharislive <mharislive@hotmail.com>
2023-02-18 13:00:21 +0530

    semantic structure and meta tags

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import Nav from "../Nav";
+
+function Header() {
+  return (
+    <header>
+      <img src="/logo.svg" alt="Little Lemon Logo" />
+      <Nav />
+    </header>
+  );
+}
+
+export default Header;
